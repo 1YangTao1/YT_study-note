fe5343675d6846be4e3e340ae049414a297e2021
YangTao <2495745362@qq.com>
2020-09-08 16:44:19 +0800

    once

---
+++ 77组合.py
@@ -0,0 +1,14 @@
+n = 4
+k = 2
+def backstark(first=1,cur=[]):
+    if len(cur)==k:
+        return res.append(cur[:])
+    else:
+        for i in range(first,n+1):
+            cur.append(i)
+            backstark(i+1,cur)
+            cur.pop()
+
+res = []
+backstark()
+print(res)
