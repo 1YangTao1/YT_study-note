5603fbf924c5efa23b699af90847f53adc7b9351
YangTao <2495745362@qq.com>
2020-09-13 16:57:51 +0800

    opencv_study

---
+++ opencv/tutorial_1.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+
+
+scr = cv.imread("D:/opencvtupian/abc.jpg")
+#读取一张图片
+cv.namedWindow("input image",cv.WINDOW_AUTOSIZE)
+#创建GUI显示图片
+cv.imshow("input image",scr)
+cv.waitKey(0)
+cv.destroyAllWindows()
